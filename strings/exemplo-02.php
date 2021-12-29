0a5638ed9ceba3f73465cd86ab469f743249d6b9
Felipe Matheus <felipematheus1106@outlook.com>
2021-12-28 22:23:34 -0400

    exemplos strings

---
+++ strings/exemplo-02.php
@@ -0,0 +1,30 @@
+<?php
+
+$nome = "Felipe Matheus";
+
+echo $nome;
+
+echo "<br>";
+
+echo strtoupper ($nome);
+//  strtupper serve pra deixa todo nome para maiusculo
+
+echo "<br>";
+
+echo strtolower($nome);
+// todo nome em minusculo
+echo"<br>";
+
+echo ucwords ( $nome);
+//deixa a primeira letra de cada palavra maiuscula
+ echo "<br>";
+ echo ucfirst($nome);
+//apenas a primeira letra da palavra ficara maiuscula
+
+
+ echo "<br>";
+
+
+
+
+?>
